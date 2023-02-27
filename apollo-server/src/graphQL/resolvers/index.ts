61306cbd8f3951a7882c8909156fce90870f0bd2
shaun <modipanesh@gmail.com>
2023-02-27 06:47:33 +0200

    moved the lib folder to src folder

--- apollo-server/src/graphQL/resolvers/index.ts
+++
@@ -1,6 +0,0 @@
-import merge from 'lodash.merge';
-import userResolvers from './users/index.js';
-
-const resolvers = merge({}, userResolvers);
-
-export default resolvers;
--- apollo-server/src/graphQL/typeDefs/index.ts
+++
@@ -1,5 +0,0 @@
-import usersTypeDefs from './users/index.js';
-
-const typeDefs = [usersTypeDefs];
-
-export default typeDefs;
