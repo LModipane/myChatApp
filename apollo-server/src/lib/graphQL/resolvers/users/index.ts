764c525a414f1e1e3eb56a4308055e3b35cffab0
shaun <modipanesh@gmail.com>
2023-02-27 11:03:56 +0200

    using prisma to update username

--- apollo-server/src/lib/graphQL/resolvers/users/index.ts
+++ apollo-server/src/lib/graphQL/resolvers/users/index.ts
@@ -1,14 +1,48 @@
-import { MyContext } from '../../../lib/@types/resolversTypes.js';
+import {
+	MyContext,
+	SubmitUsernameArgs,
+	SubmitUsernameResponse,
+} from '../../../../lib/@types/resolversTypes.js';
 
 const resolvers = {
 	Mutation: {
-		submitUsername: (_: unknown, { username }, { token }: MyContext) => {
-			console.log(token);
-			return {
-				code: 200,
-				success: true,
-				message: 'Your username has been updated successfully',
-			};
+		submitUsername: async (
+			_: unknown,
+			{ username }: SubmitUsernameArgs,
+			{ token, session }: MyContext,
+		): Promise<SubmitUsernameResponse> => {
+			if (!session) {
+				return {
+					code: 401,
+					success: false,
+					message: 'Not authenticated, please sign in',
+				};
+			}
+			// console.log(session.user.id);
+			try {
+				// const { id: myUserId } = session.user;
+				await prisma.user.update({
+					where: {
+						id: "shun"
+					}, //this prop selects the id we want to update
+					data: {
+						username,
+					}, //this prop update the username field with the username from above
+				});
+
+				return {
+					code: 200,
+					message: 'Successfully submitted username',
+					success: true,
+				};
+			} catch (error) {
+				console.log('opps, error in submitting username: ', error);
+				return {
+					code: 500,
+					success: false,
+					message: error.message,
+				};
+			}
 		},
 	},
 
